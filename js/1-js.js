77157c6835f1329939c0a00e3bc11253aded1dd4
Skripnuyk <walker8613@gmail.com>
2022-12-23 12:43:04 +0200

    added start files

---
+++ js/1-js.js
@@ -0,0 +1,13 @@
+const answer = prompt("Who is the best Wife in the world?")
+if(answer === 'I') {
+    alert("That's right!")
+}
+else if (answer === "I'm") {
+    alert("That's right!")
+}
+else if (answer === "Я") {
+    alert("That's right!")
+} 
+else {
+    alert("That's you! My beautiful!")
+}
